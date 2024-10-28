f134401f882f396a14e39c4b5e13e536adabb861
groxx20 <pavelparradomarin@gmail.com>
2024-10-29 00:32:58 +0100

    add view model + test

---
+++ app/src/main/java/com/example/tictacdoe/presentation/BoardViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.tictacdoe.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.tictacdoe.domain.UpdateBoardUseCase
+import com.example.tictacdoe.domain.VerifyBoardUseCase
+import com.example.tictacdoe.domain.model.BoardUi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class BoardViewModel(
+    verifyBoardUseCase: VerifyBoardUseCase,
+    private val updateBoardUseCase: UpdateBoardUseCase
+) : ViewModel() {
+    private val state = MutableStateFlow(BoardUi())
+    val uiState: StateFlow<BoardUi> = state
+
+    init {
+        viewModelScope.launch {
+            verifyBoardUseCase.boardUiState.collect { boardUi ->
+                state.value = boardUi
+            }
+        }
+    }
+
+    fun updateBoard(row: Int, column: Int, value: String) {
+        updateBoardUseCase.updateBoard(row, column, value)
+    }
+}
---
+++ app/src/test/java/com/example/tictacdoe/presentation/BoardViewModelTest.kt
@@ -0,0 +1,69 @@
+package com.example.tictacdoe.presentation
+
+import com.example.tictacdoe.domain.UpdateBoardUseCase
+import com.example.tictacdoe.domain.VerifyBoardUseCase
+import com.example.tictacdoe.domain.model.BoardStatus
+import com.example.tictacdoe.domain.model.BoardUi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class BoardViewModelTest {
+
+    @Mock
+    private lateinit var verifyUseCase: VerifyBoardUseCase
+
+    @Mock
+    private lateinit var updateBoardUseCase: UpdateBoardUseCase
+
+    private lateinit var viewModel: BoardViewModel
+
+    private val testDispatcher = StandardTestDispatcher()
+
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.openMocks(this)
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun `verify that view model gets board state from use case`() = runTest {
+        val boardUi = BoardUi(
+            board = listOf(
+                listOf("X", "O", "X"),
+                listOf("O", "X", "O"),
+                listOf("X", "O", "X")
+            ),
+            boardStatus = BoardStatus.WIN
+        )
+
+        `when`(verifyUseCase.boardUiState).thenReturn(flowOf(boardUi))
+
+        viewModel = BoardViewModel(verifyUseCase, updateBoardUseCase)
+
+        // Ensure to advance the dispatcher
+        testDispatcher.scheduler.advanceUntilIdle()
+
+        val result = viewModel.uiState.first()
+
+        assert(result.boardStatus == BoardStatus.WIN)
+    }
+}
